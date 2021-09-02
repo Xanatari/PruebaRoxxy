dc8de9640b5cd1713e927d94e0a2c417f7d193a9
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-02 14:44:09 -0500

    Firts Commit

---
+++ PruebaNeivorRoxxy/src/main/java/com/neivor/prueba/roxxy/PruebaNeivorRoxxyApplication.java
@@ -0,0 +1,13 @@
+package com.neivor.prueba.roxxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaNeivorRoxxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaNeivorRoxxyApplication.class, args);
+    }
+
+}
---
+++ PruebaNeivorRoxxy/src/test/java/com/neivor/prueba/roxxy/PruebaNeivorRoxxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neivor.prueba.roxxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaNeivorRoxxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
