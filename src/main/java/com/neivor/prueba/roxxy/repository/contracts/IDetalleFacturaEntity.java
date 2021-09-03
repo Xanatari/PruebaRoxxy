fe91e0a18fc0c098f990400fc85342e5b460761c
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-03 15:45:31 -0500

    Create IDetalleFacturaEntity.java

---
+++ src/main/java/com/neivor/prueba/roxxy/repository/contracts/IDetalleFacturaEntity.java
@@ -0,0 +1,7 @@
+package com.neivor.prueba.roxxy.repository.contracts;
+
+import com.neivor.prueba.roxxy.repository.entities.DetalleFacturaEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IDetalleFacturaEntity extends CrudRepository<DetalleFacturaEntity, Integer> {
+}
