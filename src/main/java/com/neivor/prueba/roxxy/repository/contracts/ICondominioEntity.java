ab3e061f179bbf1e36871497791c0e2a0cc0e087
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-03 15:45:29 -0500

    Create ICondominioEntity.java

---
+++ src/main/java/com/neivor/prueba/roxxy/repository/contracts/ICondominioEntity.java
@@ -0,0 +1,10 @@
+package com.neivor.prueba.roxxy.repository.contracts;
+
+import com.neivor.prueba.roxxy.repository.entities.CondominioEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICondominioEntity extends CrudRepository<CondominioEntity , Integer> {
+
+}
