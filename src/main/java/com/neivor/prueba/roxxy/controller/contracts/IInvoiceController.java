57cc7d5e7fe57dc8ff649822abd4936df84d5740
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 12:12:47 -0500

    Update IInvoiceController.java

--- src/main/java/com/neivor/prueba/roxxy/controller/contracts/IInvoiceController.java
+++ src/main/java/com/neivor/prueba/roxxy/controller/contracts/IInvoiceController.java
@@ -3,9 +3,13 @@ package com.neivor.prueba.roxxy.controller.contracts;
 import com.neivor.prueba.roxxy.dtos.request.InvoiceGenericRequest;
 import io.swagger.annotations.Api;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Api(value = "/invoice")
 public interface IInvoiceController {
 
     ResponseEntity<Object> registerNewInvoice(InvoiceGenericRequest invoiceGenericRequest);
+
+    ResponseEntity<Object> detailByInvoiceId(@RequestBody InvoiceGenericRequest invoiceGenericRequest);
+
 }
