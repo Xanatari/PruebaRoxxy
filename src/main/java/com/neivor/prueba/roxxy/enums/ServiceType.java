1d36e260befbc1d0f057f48e8863a28dbad75b04
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 12:50:57 -0500

    Update ServiceType.java

--- src/main/java/com/neivor/prueba/roxxy/enums/ServiceType.java
+++ src/main/java/com/neivor/prueba/roxxy/enums/ServiceType.java
@@ -1,17 +1,27 @@
 package com.neivor.prueba.roxxy.enums;
 
 public enum ServiceType {
-    JACUSII("1", "Jacussi", "Jacussi de 4 personas comunal");
+    JACUSII(1, "Jacussi", "Jacussi de 4 personas comunal");
 
-    String serviceTypeId;
+    int serviceTypeId;
     String serviceTypeName;
     String serviceTypeDetail;
 
-    ServiceType(String serviceTypeId, String serviceTypeName, String serviceTypeDetail) {
+    ServiceType(int serviceTypeId, String serviceTypeName, String serviceTypeDetail) {
         this.serviceTypeId = serviceTypeId;
         this.serviceTypeName = serviceTypeName;
         this.serviceTypeDetail = serviceTypeDetail;
     }
 
+    public int getServiceTypeId() {
+        return serviceTypeId;
+    }
 
+    public String getServiceTypeName() {
+        return serviceTypeName;
+    }
+
+    public String getServiceTypeDetail() {
+        return serviceTypeDetail;
+    }
 }
