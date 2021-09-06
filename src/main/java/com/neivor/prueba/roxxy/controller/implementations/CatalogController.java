c13507b2fd6ecc6a8f2909908bfba58088d593e3
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 12:04:29 -0500

    Create CatalogController.java

---
+++ src/main/java/com/neivor/prueba/roxxy/controller/implementations/CatalogController.java
@@ -0,0 +1,35 @@
+package com.neivor.prueba.roxxy.controller.implementations;
+
+import com.google.gson.Gson;
+import com.neivor.prueba.roxxy.constants.BusinessConstansConnector;
+import com.neivor.prueba.roxxy.controller.contracts.ICatalogsController;
+import com.neivor.prueba.roxxy.dtos.request.InvoiceGenericRequest;
+import com.neivor.prueba.roxxy.enums.Catalogtype;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+public class CatalogController implements ICatalogsController {
+
+    private static final Logger LOGGER = LogManager.getLogger(CatalogController.class);
+    private static final Gson gson = new Gson();
+
+    @Override
+    public ResponseEntity<Object> registerNewCatalog(@RequestHeader (BusinessConstansConnector.HEADER_CATALOG_TYPE) Catalogtype catalogtype , InvoiceGenericRequest invoiceGenericRequest) {
+        LOGGER.info("Start process to register Catalog name {}",catalogtype.getCatalogType() );
+
+        
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> getAllcatalogs() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> getdetailByCatalog() {
+        return null;
+    }
+}
