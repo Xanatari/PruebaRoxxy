41fd4f51c5b83795a2650e85c94c16a11b39fc3f
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 16:42:39 -0500

    Update IInvoiceService.java

--- src/main/java/com/neivor/prueba/roxxy/service/contracts/IInvoiceService.java
+++ src/main/java/com/neivor/prueba/roxxy/service/contracts/IInvoiceService.java
@@ -1,9 +1,10 @@
 package com.neivor.prueba.roxxy.service.contracts;
 
 import com.neivor.prueba.roxxy.dtos.request.InvoiceGenericRequest;
+import com.neivor.prueba.roxxy.exceptions.InvocieNotFoundException;
 
 public interface IInvoiceService {
     int registerRecord(InvoiceGenericRequest invoiceGenericRequest);
 
-
+    void updateInvoice(InvoiceGenericRequest invoiceGenericRequest)throws InvocieNotFoundException;
 }
