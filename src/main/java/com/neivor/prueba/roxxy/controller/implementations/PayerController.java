6f7df2c5ef9b37f5b544ea3916196b30649ceaa2
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 16:16:40 -0500

    Update PayerController.java

--- src/main/java/com/neivor/prueba/roxxy/controller/implementations/PayerController.java
+++ src/main/java/com/neivor/prueba/roxxy/controller/implementations/PayerController.java
@@ -6,19 +6,18 @@ import com.neivor.prueba.roxxy.dtos.request.PayerGenericRequest;
 import com.neivor.prueba.roxxy.dtos.responses.InvocieResponse;
 import com.neivor.prueba.roxxy.dtos.responses.NeivorResponse;
 import com.neivor.prueba.roxxy.exceptions.GenericException;
+import com.neivor.prueba.roxxy.exceptions.InvocieNotFoundException;
 import com.neivor.prueba.roxxy.exceptions.InvoiceAmmountErrException;
 import com.neivor.prueba.roxxy.exceptions.InvoiceNationalIdNotFoundException;
 import com.neivor.prueba.roxxy.service.contracts.IPayerService;
+import com.sun.istack.NotNull;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/payer", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -63,7 +62,25 @@ public class PayerController implements IPayerController {
     }
 
     @Override
-    public ResponseEntity<Object> detailByPayerId(@RequestBody PayerGenericRequest payerRequest) {
-        return null;
+    @GetMapping(path = "/detail/{invoiceId}")
+    public ResponseEntity<Object> detailByPayerId(@RequestBody PayerGenericRequest payerRequest,
+                                                  @PathVariable @NotNull int invoiceId) {
+
+        try{
+            LOGGER.info("Start process to register Catalog name {}");
+            return new ResponseEntity<>(gson.toJson(NeivorResponse.builder()
+                    .rc("0")
+                    .msg("OK")
+                    .data(iPayerService.payerInfo(invoiceId))
+                    .build()
+            ), HttpStatus.OK);
+
+        }catch ( InvocieNotFoundException invocieNotFoundException){
+            LOGGER.error("Error to register the Payer info ");
+            return new ResponseEntity<>(gson.toJson(invocieNotFoundException), HttpStatus.BAD_REQUEST);
+        }catch (Exception e ){
+            LOGGER.error("Error to register the Payer info ");
+            return new ResponseEntity<>(gson.toJson(new GenericException("Have error plis try again")), HttpStatus.BAD_REQUEST);
+        }
     }
 }
