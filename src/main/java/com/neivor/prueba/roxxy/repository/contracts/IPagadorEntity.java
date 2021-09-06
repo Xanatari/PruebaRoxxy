5d2f516f82232cec4115c8cd4075a0afe0ac3acb
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 16:16:43 -0500

    Update IPagadorEntity.java

--- src/main/java/com/neivor/prueba/roxxy/repository/contracts/IPagadorEntity.java
+++ src/main/java/com/neivor/prueba/roxxy/repository/contracts/IPagadorEntity.java
@@ -4,6 +4,9 @@ import com.neivor.prueba.roxxy.repository.entities.PagadorEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IPagadorEntity extends CrudRepository<PagadorEntity, Integer> {
+    Optional<PagadorEntity> findByDocumentoPagador(String documentoPagador);
 }
