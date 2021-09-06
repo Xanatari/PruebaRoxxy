d105601ddfeca83829dd517f38038cac043e821c
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-05 23:12:27 -0500

    Create RestConstansCategory.java

---
+++ src/main/java/com/neivor/prueba/roxxy/constants/RestConstansCategory.java
@@ -0,0 +1,6 @@
+package com.neivor.prueba.roxxy.constants;
+
+public class RestConstansCategory {
+    public static final String CATEGORY_GENERAL = "General";
+    public static final String CATEGORY_INVOICE_DETAIL = "API's Invoice detail";
+}
