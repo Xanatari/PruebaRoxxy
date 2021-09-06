025beea6233b74bd5df05571e5e73b039bb5fdff
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 14:53:07 -0500

    Update IFacturaEntity.java

--- src/main/java/com/neivor/prueba/roxxy/repository/contracts/IFacturaEntity.java
+++ src/main/java/com/neivor/prueba/roxxy/repository/contracts/IFacturaEntity.java
@@ -3,7 +3,13 @@ package com.neivor.prueba.roxxy.repository.contracts;
 import com.neivor.prueba.roxxy.repository.entities.FacturaEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface IFacturaEntity extends CrudRepository<FacturaEntity, Integer> {
+
+    @Transactional(readOnly = true)
+    Optional<FacturaEntity> findByDocPagador(String docPagador);
 }
