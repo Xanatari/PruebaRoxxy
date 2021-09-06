6c5280f250ec84f5bb5e11725503fbbe84626a57
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 14:53:05 -0500

    Create InvoiceNationalIdNotFoundException.java

---
+++ src/main/java/com/neivor/prueba/roxxy/exceptions/InvoiceNationalIdNotFoundException.java
@@ -0,0 +1,5 @@
+package com.neivor.prueba.roxxy.exceptions;
+
+public class InvoiceNationalIdNotFoundException extends Exception{
+    public InvoiceNationalIdNotFoundException(String message){super(message);}
+}
