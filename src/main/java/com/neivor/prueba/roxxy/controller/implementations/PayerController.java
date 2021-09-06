e2b1729fc477e63a8625e900f53e445fc3e0808f
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 14:06:43 -0500

    Update PayerController.java

--- src/main/java/com/neivor/prueba/roxxy/controller/implementations/PayerController.java
+++ src/main/java/com/neivor/prueba/roxxy/controller/implementations/PayerController.java
@@ -3,8 +3,10 @@ package com.neivor.prueba.roxxy.controller.implementations;
 import com.google.gson.Gson;
 import com.neivor.prueba.roxxy.controller.contracts.IPayerController;
 import com.neivor.prueba.roxxy.dtos.request.PayerGenericRequest;
+import com.neivor.prueba.roxxy.service.contracts.IPayerService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,9 +20,18 @@ public class PayerController implements IPayerController {
     private static final Logger LOGGER = LogManager.getLogger(PayerController.class);
     private static final Gson gson = new Gson();
 
+    IPayerService iPayerService;
+
+    @Autowired
+    public PayerController(IPayerService iPayerService) {
+        this.iPayerService = iPayerService;
+    }
+
     @Override
     public ResponseEntity<Object> registerNewPayer(@RequestBody PayerGenericRequest payerRequest) {
         LOGGER.info("Start process to register Catalog name {}");
+
+
         return null;
     }
 
