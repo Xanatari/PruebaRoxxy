01ea2f528e702f007c6e427284ea862e1dad5a1d
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 16:48:15 -0500

    Update InvoiceStatus.java

--- src/main/java/com/neivor/prueba/roxxy/enums/InvoiceStatus.java
+++ src/main/java/com/neivor/prueba/roxxy/enums/InvoiceStatus.java
@@ -2,7 +2,8 @@ package com.neivor.prueba.roxxy.enums;
 
 public enum InvoiceStatus {
     PENDING(1),
-    PAY_AND_PENDING_REVIEW(2);
+    PAY_AND_PENDING_REVIEW(2),
+    REVERSE_PAY(4);
 
     private final int invoiceStatus;
 
