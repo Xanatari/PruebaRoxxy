6b3d4aee3a3a187a3ecbaa77e9b8e4e515116cff
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 14:53:09 -0500

    Update IPayerService.java

--- src/main/java/com/neivor/prueba/roxxy/service/contracts/IPayerService.java
+++ src/main/java/com/neivor/prueba/roxxy/service/contracts/IPayerService.java
@@ -1,7 +1,9 @@
 package com.neivor.prueba.roxxy.service.contracts;
 
 import com.neivor.prueba.roxxy.dtos.request.PayerGenericRequest;
+import com.neivor.prueba.roxxy.exceptions.InvoiceAmmountErrException;
+import com.neivor.prueba.roxxy.exceptions.InvoiceNationalIdNotFoundException;
 
 public interface IPayerService {
-    int registerPayer(PayerGenericRequest payerRequest);
+    void registerPayer(PayerGenericRequest payerRequest) throws InvoiceNationalIdNotFoundException, InvoiceAmmountErrException;
 }
