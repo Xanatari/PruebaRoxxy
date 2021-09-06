f07428f2778201d7dcb8a050bda2951122ab759a
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 13:38:28 -0500

    Update IPayerController.java

--- src/main/java/com/neivor/prueba/roxxy/controller/contracts/IPayerController.java
+++ src/main/java/com/neivor/prueba/roxxy/controller/contracts/IPayerController.java
@@ -1,6 +1,7 @@
 package com.neivor.prueba.roxxy.controller.contracts;
 
 import com.neivor.prueba.roxxy.dtos.request.InvoiceGenericRequest;
+import com.neivor.prueba.roxxy.dtos.request.PayerGenericRequest;
 import com.neivor.prueba.roxxy.enums.Catalogtype;
 import io.swagger.annotations.Api;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,6 @@ import org.springframework.http.ResponseEntity;
 @Api(value = "/payer")
 public interface IPayerController {
 
-    ResponseEntity<Object> registerNewPayer( InvoiceGenericRequest invoiceGenericRequest);
-    ResponseEntity<Object> detailByPayerId( InvoiceGenericRequest invoiceGenericRequest);
+    ResponseEntity<Object> registerNewPayer( PayerGenericRequest payerGenericRequest);
+    ResponseEntity<Object> detailByPayerId( PayerGenericRequest payerGenericRequest);
 }
