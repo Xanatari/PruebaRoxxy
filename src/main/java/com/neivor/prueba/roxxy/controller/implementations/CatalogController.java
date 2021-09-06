b2630cb65b169c3818fe5f353f6d5a211985bad9
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 12:12:52 -0500

    Update CatalogController.java

--- src/main/java/com/neivor/prueba/roxxy/controller/implementations/CatalogController.java
+++ src/main/java/com/neivor/prueba/roxxy/controller/implementations/CatalogController.java
@@ -7,19 +7,24 @@ import com.neivor.prueba.roxxy.dtos.request.InvoiceGenericRequest;
 import com.neivor.prueba.roxxy.enums.Catalogtype;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(path = "/catalog", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class CatalogController implements ICatalogsController {
 
     private static final Logger LOGGER = LogManager.getLogger(CatalogController.class);
     private static final Gson gson = new Gson();
 
     @Override
-    public ResponseEntity<Object> registerNewCatalog(@RequestHeader (BusinessConstansConnector.HEADER_CATALOG_TYPE) Catalogtype catalogtype , InvoiceGenericRequest invoiceGenericRequest) {
-        LOGGER.info("Start process to register Catalog name {}",catalogtype.getCatalogType() );
+    public ResponseEntity<Object> registerNewCatalog(@RequestHeader(BusinessConstansConnector.HEADER_CATALOG_TYPE) Catalogtype catalogtype, InvoiceGenericRequest invoiceGenericRequest) {
+        LOGGER.info("Start process to register Catalog name {}", catalogtype.getCatalogType());
 
-        
+
         return null;
     }
 
