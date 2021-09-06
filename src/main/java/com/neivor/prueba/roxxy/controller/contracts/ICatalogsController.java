454d0188c1f55cbec6cb42a42de51b4de66b3bb7
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 12:04:28 -0500

    Create ICatalogsController.java

---
+++ src/main/java/com/neivor/prueba/roxxy/controller/contracts/ICatalogsController.java
@@ -0,0 +1,14 @@
+package com.neivor.prueba.roxxy.controller.contracts;
+
+import com.neivor.prueba.roxxy.dtos.request.InvoiceGenericRequest;
+import com.neivor.prueba.roxxy.enums.Catalogtype;
+import io.swagger.annotations.Api;
+import org.springframework.http.ResponseEntity;
+
+@Api(value = "/catalogs")
+public interface ICatalogsController {
+
+    ResponseEntity<Object> registerNewCatalog(Catalogtype catalogtype, InvoiceGenericRequest invoiceGenericRequest);
+    ResponseEntity<Object> getAllcatalogs();
+    ResponseEntity<Object> getdetailByCatalog();
+}
