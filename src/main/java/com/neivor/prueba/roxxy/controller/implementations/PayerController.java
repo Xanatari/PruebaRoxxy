c1533bf73558f6cc50b4f2de097a64d04f0638f3
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2021-09-06 13:43:37 -0500

    Update PayerController.java

--- src/main/java/com/neivor/prueba/roxxy/controller/implementations/PayerController.java
+++ src/main/java/com/neivor/prueba/roxxy/controller/implementations/PayerController.java
@@ -2,12 +2,12 @@ package com.neivor.prueba.roxxy.controller.implementations;
 
 import com.google.gson.Gson;
 import com.neivor.prueba.roxxy.controller.contracts.IPayerController;
-import com.neivor.prueba.roxxy.dtos.request.InvoiceGenericRequest;
 import com.neivor.prueba.roxxy.dtos.request.PayerGenericRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +19,13 @@ public class PayerController implements IPayerController {
     private static final Gson gson = new Gson();
 
     @Override
-    public ResponseEntity<Object> registerNewPayer(PayerGenericRequest payerRequest) {
-        LOGGER.info("Start process to register Catalog name {}", catalogtype.getCatalogType());
+    public ResponseEntity<Object> registerNewPayer(@RequestBody PayerGenericRequest payerRequest) {
+        LOGGER.info("Start process to register Catalog name {}");
         return null;
     }
 
     @Override
-    public ResponseEntity<Object> detailByPayerId(PayerGenericRequest payerRequest) {
+    public ResponseEntity<Object> detailByPayerId(@RequestBody PayerGenericRequest payerRequest) {
         return null;
     }
 }
