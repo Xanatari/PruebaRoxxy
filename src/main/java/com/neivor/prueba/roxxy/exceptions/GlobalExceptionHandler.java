fda734db3a0e0c54a07f0a4ed231a2361cdf0840
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2025-02-18 19:36:48 -0500

    jj

---
+++ src/main/java/com/neivor/prueba/roxxy/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.neivor.prueba.roxxy.exceptions;
+
+import com.neivor.prueba.roxxy.dtos.responses.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(InvoiceNationalIdNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleInvoiceNationalIdNotFoundException(InvoiceNationalIdNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
